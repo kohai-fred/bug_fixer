88a89c107da4092d05019d76d9523f97af630f7e
kohai-fred <frederick.vu.pro@gmail.com>
2022-10-27 13:25:21 +0200

    Fix import apiUrl.js in github pages

--- services/request/APIservice.js
+++ services/request/APIservice.js
@@ -1,7 +1,7 @@
 import { getLocalStorage } from "../../JS/utils/getLocalStorage.js";
-import { API_URL } from "./apiUrl.js";
+// import { API_URL } from "./apiUrl.js";
 
-const URL = API_URL;
+const URL = "http://greenvelvet.alwaysdata.net/bugTracker/api";
 const { id, token } = getLocalStorage();
 
 const instance = axios.create({
